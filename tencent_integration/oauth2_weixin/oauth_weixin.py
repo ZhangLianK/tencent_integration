ca752e9883356bac304623a107901a484f17d500
ZhangLianK <zhangliankun0907@foxmail.com>
2023-12-07 06:48:54 +0800

    add debug message

--- tencent_integration/oauth2_weixin/oauth_weixin.py
+++ tencent_integration/oauth2_weixin/oauth_weixin.py
@@ -154,6 +154,8 @@ def get_info_via_oauth(
 		api_endpoint_args = oauth2_providers[provider].get("api_endpoint_args")
 		frappe.log_error('session token key',session.access_token_key)
 		frappe.log_error('session token',session.access_token)
+		#add access_token to api_endpoint_args
+		api_endpoint_args['access_token'] = session.access_token
 		info = session.get(api_endpoint, params=api_endpoint_args).json()
 		frappe.log_error('oauth info',info)
 		if provider == "github" and not info.get("email"):
