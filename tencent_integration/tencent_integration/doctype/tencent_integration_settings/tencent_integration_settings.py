48770ed5a273330660ca7f54012fcf99349e60b3
ZhangLianK <zhangliankun0907@foxmail.com>
2023-12-18 16:37:31 +0800

    fix

--- tencent_integration/tencent_integration/doctype/tencent_integration_settings/tencent_integration_settings.py
+++ tencent_integration/tencent_integration/doctype/tencent_integration_settings/tencent_integration_settings.py
@@ -4,6 +4,6 @@ from frappe import _
 
 class TencentIntegrationSettings(Document):
     def before_submit(self):
-        api_info = frappe.get_all('Tencent Integration Settings', filters={'company': self.company, 'action':'SendSms', 'docstatus':1}, limit_page_length=1)
+        api_info = frappe.get_all('Tencent Integration Settings', filters={'company': self.company, 'action':self.action, 'docstatus':1}, limit_page_length=1)
         if api_info:
-            frappe.throw(_("Tencent Integration Settings for SMS already exists for the company {0}, Docname {1}").format(self.company, api_info[0].name)) 
+            frappe.throw(_("Tencent Integration Settings for {2} already exists for the company {0}, Docname {1}").format(self.company, api_info[0].name, self.action)) 
