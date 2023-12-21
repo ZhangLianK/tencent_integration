76848c95349a5dcaf51c27ba785a052f00f23066
ZhangLianK <zhangliankun0907@foxmail.com>
2023-12-21 20:56:02 +0800

    fix

--- tencent_integration/tencent_integration/doctype/tencent_integration_security/tencent_integration_security.py
+++ tencent_integration/tencent_integration/doctype/tencent_integration_security/tencent_integration_security.py
@@ -4,7 +4,7 @@ from frappe import _
 
 class TencentIntegrationSecurity(Document):
     def before_submit(self):
-        secret_info = frappe.get_all('Tencent Integration Security', filters={'company': self.company, 'action':'SendSms', 'docstatus':1}, limit_page_length=1)
+        secret_info = frappe.get_all('Tencent Integration Security', filters={'company': self.company, 'action':self.action, 'docstatus':1}, limit_page_length=1)
         if secret_info:
-            frappe.throw(_("Tencent Integration Security for SMS already exists for the company {0}").format(self.company)) 
+            frappe.throw(_("Tencent Integration Security for {1} already exists for the company {0}").format(self.company, self.action)) 
             
